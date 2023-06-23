172941def9a2f172ddeefaf8b4154527b6754a24
Harsh Soni <harsh.here17@gmail.com>
2023-06-23 10:41:25 +0530

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Harsh Soni on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
